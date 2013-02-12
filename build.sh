e3248ffc0785629f0ed93fdd1a802297eb414927
antdking <anthonydking@gmail.com>
2013-02-12 22:05:21 +0000

    update our scripts do we seriously want to add support for another device in our kernels?

--- build.sh
+++ build.sh
@@ -13,20 +13,12 @@ vendor=lge
 
 # changed Ron "Readded options. This have hazel board as well" 05/02/13
 if [ -z $target ]; then
-echo "choose your target device"
-echo "1) p350"
-echo "2) c500"
-read -p "1/2: " choice
-case "$choice" in
-1 ) export target=p350
+export target=p350
 if [ -f arch/arm/configs/cyanogenmod_p350_defconfig ]; then
 export defconfig=cyanogenmod_p350_defconfig
 else
 export defconfig=cyanogenmod_pecan_defconfig
-fi ;;
-2 ) export target=c500 ; export defconfig=cyanogenmod_hazel_defconfig;;
-* ) echo "invalid choice"; sleep 2 ; ./build.sh;;
-esac
+fi
 fi # [ -z $target ]
 if [ -z $compiler ]; then
 if [ -f ../arm-eabi-4.6/bin/arm-eabi-* ]; then
@@ -72,7 +64,7 @@ cp arch/arm/boot/zImage zip-creator/kernel
 find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/
 
 
-zipfile="PecanCM.x-$target-$daytime.zip"
+zipfile="PecanCM.35-$target-$daytime.zip"
 cd zip-creator
 rm -f *.zip
 zip -r $zipfile * -x *kernel/.gitignore*
