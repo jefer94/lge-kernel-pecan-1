757b44c1bbd534bebf8a817f40e511dd07af5173
Jan Engelhardt <jengelh@medozas.de>
2013-02-19 16:20:11 +0530

    netfilter: xtables: add missing aliases for autoloading via iptables
    Change-Id: Iafbf192811f19034f478d13f87f1723c873b5ecb
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Tue Feb 19 14:23:52 2013
+ * Tue Feb 19 16:18:06 2013
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
