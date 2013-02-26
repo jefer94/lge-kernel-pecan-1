b33cf9f0d8b4209f5fc14471d646b0cc1e07ef7b
Ken Zhang <kenz@codeaurora.org>
2013-02-26 21:01:36 +0530

    msm: display: fb blend setting via MSMFB_METADATA_SET ioctl
    MSMFB_METADATA_SET ioctl is added for the fb user to pass
    in additional infomation rather than using reserved
    fields.
    User can set pre-multiplied alpha for the frame buffer via this
    ioctl, overlay blend setting will take this into account.
    
    Change-Id: Ic2f6236d84691fb03174ec479874c6928489ea30

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Tue Feb 26 13:38:00 2013
+ * Tue Feb 26 20:51:26 2013
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
