58dc7bcc24edf344bfc52d499047f6cebb8ce558
Nagamalleswararao Ganji <nganji@codeaurora.org>
2013-02-26 11:27:24 +0530

    msm: ion heap support for the mdp driver
    heap id is added to mdp driver pdata as driver needs to allocate
    the memory from different heap based on the msm target
    
    Change-Id: I60d163b663f56933fc0360cc6fa48edf2d0bb3c7
    Signed-off-by: Nagamalleswararao Ganji <nganji@codeaurora.org>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -226,6 +226,9 @@ struct msm_panel_common_pdata {
         int *mdp_core_clk_table;
         int mdp_rev;
         int (*writeback_offset)(void);
+        u32 ov0_wb_size;  /* overlay0 writeback size */
+	u32 ov1_wb_size;  /* overlay1 writeback size */
+	u32 mem_hid;
 };
 
 struct lcdc_platform_data {
