b286255ebb249699a89af990a71e54edbb443bc7
RonGokhale <cips173@gmail.com>
2013-03-04 13:13:51 +0530

    cpufreq: interactive:  remove //
    Change-Id: I00c47229183f69a725ff4594972e96cc954485bb
    Signed-off-by: RonGokhale <cips173@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -159,8 +159,8 @@ static void cpufreq_interactive_timer(unsigned long data)
 	if (cpufreq_frequency_table_target(pcpu->policy, pcpu->freq_table,
 					   new_freq, CPUFREQ_RELATION_H,
 					   &index)) {
-//		pr_warn_once("timer %d: cpufreq_frequency_table_target error\n",
-//			     (int) data);
+		pr_warn_once("timer %d: cpufreq_frequency_table_target error\n",
+			     (int) data);
 		goto rearm;
 	}
 
