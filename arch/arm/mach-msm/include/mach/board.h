20d40065d429354fd451257bf2fa5e2e15f4f332
Kuogee Hsieh <khsieh@codeaurora.org>
2013-03-04 10:27:40 +0530

    msm_fb: display: add writeback_offset to struct msm_panel_common_pdata
    Change-Id: I6c0625e33b965180ece5d17ecd1955bca01a9730
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -277,6 +277,7 @@ struct msm_panel_common_pdata {
 	unsigned num_mdp_clk;
 	int *mdp_core_clk_table;
         int mdp_rev;
+        int (*writeback_offset)(void);
 #ifdef CONFIG_MSM_BUS_SCALING
 	struct msm_bus_scale_pdata *mdp_bus_scale_table;
 #endif
