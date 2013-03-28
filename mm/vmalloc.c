1d16e16fe4ba85490d29bd2a1052ec5dff23758b
RonGokhale <cips173@gmail.com>
2013-03-28 16:50:32 +0530

    Updated defconfig for linux 3.0.58
    Change-Id: I2774003fb26cc525a5fefb09daab794b4e958d12

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -256,6 +256,7 @@ struct vmap_area {
 	struct rb_node rb_node;		/* address sorted rbtree */
 	struct list_head list;		/* address sorted list */
 	struct list_head purge_list;	/* "lazy purge" list */
+        struct vm_struct *vm;
 	void *private;
 	struct rcu_head rcu_head;
 };
