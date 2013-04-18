672114b86ee0b08721b3e1f5b0c55bb02eeccb5b
Jeevan Shriram <jshriram@codeaurora.org>
2013-04-18 16:51:33 +0530

    msm_fb: display: add spinlock to protect dma status.
    During dma transfer to interface host the status of the dma
    needs to be protected in the smp environment.
    
    Change-Id: I02b4f046de7f693bb098343b7345c4aa6ca395bd
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -1882,9 +1882,11 @@ irqreturn_t mdp_isr(int irq, void *ptr)
 			}
 #ifndef CONFIG_FB_MSM_MDP303
 			dma = &dma2_data;
+			spin_lock_irqsave(&mdp_spin_lock, flag);
 			dma->busy = FALSE;
+			spin_unlock_irqrestore(&mdp_spin_lock, flag);
 			mdp_pipe_ctrl(MDP_DMA2_BLOCK, MDP_BLOCK_POWER_OFF,
-				      TRUE);
+				TRUE);
 			complete(&dma->comp);
 #else
 #ifdef CONFIG_FB_MSM_EBI2
